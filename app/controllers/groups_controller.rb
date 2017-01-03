665f8e5bf02e84c1a8b9a39c23b8a9e53a3af5c9
solarmax <solarmax1027@gmail.com>
2017-01-03 17:43:35 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @posts = @group.posts
   end
-  
+
   def edit
   end
 
@@ -33,33 +33,35 @@ class GroupsController < ApplicationController
     end
 
 
-   def update
-     if @group.update(group_params)
-       redirect_to groups_path, notice:"Update Success"
-     else
-       render :edit
-     end
-   end
+    def update
+        if @group.update(group_params)
+          redirect_to groups_path, notice: "Update Success"
+        else
+          render :edit
+        end
+      end
 
-   def destroy
-     @group.destroy
-     redirect_to groups_path, alert: "Group deleted"
-   end
+      def destroy
+         @group.destroy
+         redirect_to groups_path, alert: "Group deleted"
+       end
+
 
 
 
     private
 
 
-    def find_group_and_check_permisson
-      @group = Group.find(params[:id])
+    def find_group_and_check_permission
+        @group = Group.find(params[:id])
 
-      if current_user != @group.user
-        redirect_to root_path, alert: "You have no permission."
+        if current_user != @group.user
+          redirect_to root_path, alert: "You have no permission."
+        end
       end
-    end
 
-   def group_params
-     params.require(:group).permit(:title, :description)
-   end
-end
+      def group_params
+        params.require(:group).permit(:title, :description)
+      end
+
+    end
